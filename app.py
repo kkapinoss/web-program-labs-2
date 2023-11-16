154a48454864e4d2a6f90869410fd4ef2d030b91
Данёк <kkapinoss@mail.ru>
2023-11-16 14:06:06 +0700

    Добавлены правки для вывода пользователя после успешной авторизации

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import session
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 from lab5 import lab5
 
 # Секретный ключ, который обеспечит безопасность генерируемого JWT-токена
@@ -12,4 +13,5 @@ app.secret_key = '321'
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 app.register_blueprint(lab5)
--- lab4.py
+++ lab4.py
@@ -5,3 +5,30 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login_4.html")
+    
+    #Получение введенных значений 
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Проверка на пустой логин и пароль
+    if not username:
+        error = 'Не введен логин'
+        return render_template('login_4.html', error=error)
+    elif not password:
+        error = 'Не введен пароль'
+        return render_template('login_4.html', error=error)
+
+    #Если данные введены корректно 
+    if username == 'alex' and password == '123':
+        return render_template('success_4.html', username=username)
+    #Если данные введены некорректно 
+    else:
+        error = 'Неверный логин или пароль'
+        return render_template('login_4.html', error=error, username=username, password=password)
+    
