c3c5ec76e0caee26c2d20127abad7ba53562fc82
Данёк <kkapinoss@mail.ru>
2024-01-12 14:47:27 +0700

    Редактирование курса по REST

--- lab8.py
+++ lab8.py
@@ -33,3 +33,13 @@ def get_courses3(course_num):
         return '', 204
      else:
          return abort(404)
+     
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
+def put_courses3(course_num):
+    course=request.get_json()
+    courses[course_num]=course
+    
+    if 0<= course_num <len(courses)-1:
+        return courses[course_num] 
+    else:
+        return abort(404)
